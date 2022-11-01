47c592696ccee02e0c722bd84e9516c095d007e5
soniasalido <soniasalido@gmail.com>
2022-11-01 09:18:11 +0100

    modificando el proyecto para deploy simple

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/laboratorio-svelte-lemoncode/",
+  base: "/laboratorio-svelte-deploy/",
   plugins: [svelte()]
 })
